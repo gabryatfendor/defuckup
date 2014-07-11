a3e3d92ea5b6caf974887d8bda0b5d1387a0a526
gabryatfendor <gabrielesani@programmer.net>
2014-07-11 11:48:22 +0200

    Create defuckup.sh

---
+++ defuckup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+sourcedir=`pwd`
+OIFS="$IFS"
+IFS=$'\n'
+for f in `find . -type d -links 2`
+do
+######BASIC STEP######
+#
+cd $f
+echo "changed dir to $f"
+foldername=${PWD##*/}
+filename=f$foldername
+cat * > $filename
+echo "merged $foldername"
+mv $filename ..
+rm *
+cd ..
+rmdir $foldername
+mv $filename $foldername
+echo "removed dir $foldername, back to the source"
+cd $sourcedir
+#########################
+done
